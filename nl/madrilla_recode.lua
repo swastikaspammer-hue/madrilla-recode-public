c87264d79addef24f37ec07b7406f8c3475fd207
swastikaspammer-hue <swastikaspammer@gmail.com>
2026-07-06 01:51:25 -0700

    Fix: Disabled PowerShell progress bar rendering which was causing downloads to take 12 seconds instead of <1 second

--- nl/madrilla_recode.lua
+++ nl/madrilla_recode.lua
@@ -8072,8 +8072,7 @@ local function fetch_random_image(prefetch)
     debug_status = "Starting PowerShell..."
     
     local temp_path = "nl/goon_corner/temp_slideshow.png"
-
-    local ps_cmd = string.format('powershell -windowstyle hidden -command "Remove-Item -Path \'%s*\' -ErrorAction SilentlyContinue; Remove-Item -Path \'nl/goon_corner/error.txt\' -ErrorAction SilentlyContinue; try { [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing -TimeoutSec 20 -Uri \'%s\' -OutFile \'%s.tmp\'; Move-Item -Force \'%s.tmp\' \'%s\' } catch { Set-Content -Path \'nl/goon_corner/error.txt\' -Value $_.Exception.Message }"', temp_path, url, temp_path, temp_path, temp_path)
+    local ps_cmd = string.format('powershell -windowstyle hidden -command "Remove-Item -Path \'%s*\' -ErrorAction SilentlyContinue; Remove-Item -Path \'nl/goon_corner/error.txt\' -ErrorAction SilentlyContinue; try { $ProgressPreference = \'SilentlyContinue\'; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing -TimeoutSec 20 -Uri \'%s\' -OutFile \'%s.tmp\'; Move-Item -Force \'%s.tmp\' \'%s\' } catch { Set-Content -Path \'nl/goon_corner/error.txt\' -Value $_.Exception.Message }"', temp_path, url, temp_path, temp_path, temp_path)
     
     -- Execute asynchronously via WinExec (0 = SW_HIDE)
     pcall(function()
